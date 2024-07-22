bd9deea416cad81c623348bf8632924e9f70ea75
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-07-22 12:20:50 +0300

    update on get project items

--- src/modules/projects/controllers/project.controller.ts
+++ src/modules/projects/controllers/project.controller.ts
@@ -1,20 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ProjectService } from '../services/project.service';
 import { Project } from '@entities';
-import { EntityCrudOptions } from 'src/shared/types/crud-option.type';
-import { EntityCrudController } from 'src/shared/controller';
+import { ExtraCrudOptions } from 'src/shared/types/crud-option.type';
+import { ExtraCrudController } from 'src/shared/controller';
 import { CreateProjectDto } from '../dtos/project.dto';
 
-const options: EntityCrudOptions = {
+const options: ExtraCrudOptions = {
+  entityIdName: 'quotationId',
   createDto: CreateProjectDto,
 };
 
 @ApiBearerAuth()
 @Controller('projects')
 @ApiTags('Projects')
-export class ProjectController extends EntityCrudController<Project>(options) {
+export class ProjectController extends ExtraCrudController<Project>(options) {
   constructor(private readonly projectService: ProjectService) {
     super(projectService);
+  }
+
+  @Get('/items/:quotationId')
+  async fetchItemsByQuotation(@Param('quotationId') quotationId: string) {
+    return await this.projectService.fetchItemsByQuotation(quotationId);
   }
 }
--- src/modules/projects/services/project.service.ts
+++ src/modules/projects/services/project.service.ts
@@ -1,15 +1,26 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
-import { EntityCrudService } from 'src/shared/service';
+import { ExtraCrudService } from 'src/shared/service';
 import { Project } from 'src/entities';
 
 @Injectable()
-export class ProjectService extends EntityCrudService<Project> {
+export class ProjectService extends ExtraCrudService<Project> {
   constructor(
     @InjectRepository(Project)
     private readonly repositoryProject: Repository<Project>,
   ) {
     super(repositoryProject);
+  }
+
+  async fetchItemsByQuotation(quotationId: string) {
+    return await this.repositoryProject.find({
+      where: {
+        quotationId: quotationId,
+      },
+      relations: {
+        projectItems: true,
+      },
+    });
   }
 }
