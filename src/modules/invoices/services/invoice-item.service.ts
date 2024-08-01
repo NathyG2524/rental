8b0a638000c879606bc425b338f34a875dd211bd
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-08-01 15:54:19 +0300

    Update invoice-item.service.ts

--- src/modules/invoices/services/invoice-item.service.ts
+++ src/modules/invoices/services/invoice-item.service.ts
@@ -22,18 +22,4 @@ export class InvoiceItemService extends ExtraCrudService<InvoiceItem> {
 
     return result;
   }
-
-  groupBy(list: any, keyGetter: any) {
-    const map = new Map();
-    list.forEach((item: any) => {
-      const key = keyGetter(item);
-      const collection = map.get(key);
-      if (!collection) {
-        map.set(key, [item]);
-      } else {
-        collection.push(item);
-      }
-    });
-    return map;
-  }
 }
